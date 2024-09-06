397c99815c45b81a1df39c6fd2af0febdb0a9571
Kristina Faye <hellokristinafaye@gmail.com>
2024-09-05 20:28:51 -0700

    local storage works!

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
 let notes = document.querySelectorAll('.input-box');
 
+// pulling from local storage
+
+function showNotes() { 
+    notesContainer.innerHTML = localStorage.getItem('notes');
+}
+showNotes()
+
+// for adding to local storage
+
+function updateStorage() {
+    localStorage.setItem('notes', notesContainer.innerHTML);
+}
+
+// create notes button functionality 
+
 createBtn.addEventListener('click', () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -16,5 +31,14 @@ createBtn.addEventListener('click', () => {
 notesContainer.addEventListener('click', function(e) { 
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage()
+    }
+    else if(e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
     }
 })
